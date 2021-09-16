9f1225bac5a294516c115acb9f4ad90c8864cf87
Игнатенков Илья Сергеевич <i.ignatenkov@univ.priv>
2021-09-16 15:17:27 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,14 @@
 class Hello{
     public static void main(String[] args) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         System.out.println("Hello World!1");
 =======
         System.out.println("Hello World!");
 >>>>>>> c02d94e (Update Hello.java)
+=======
+        System.out.println("Hello World!\n");
+>>>>>>> 3918f72 (Update Hello.java in the 2th develop.)
 	System.out.println("New Future!");
     }
 }
